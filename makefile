31e9739ce2df577ee6b723cd6d0a2ec9af908770
HunterSun2018 <sht@palliums.org>
2020-10-23 10:35:06 +0800

    first commit

---
+++ makefile
@@ -0,0 +1,11 @@
+#
+# g++ version must be greater than 10.0
+#
+SRC = src/main.cpp
+TARGET = demo
+
+demo : $(SRC)
+	g++ $(SRC) -o $(TARGET) -std=c++20
+
+clean : 
+	rm demo
---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file main.cpp
+ * @author Hunter Sun (HunterSun2018@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-10-23
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ * build : g++ main.cpp -o demo -std=c++20
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ranges>
+
+using namespace std;
+
+void run_range_demo();
+
+int main(int argc, const char *argv[])
+{
+	try
+	{
+		run_range_demo();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "exception : " << e.what() << '\n';
+	}
+	
+}
+
+void run_range_demo()
+{
+	vector<int> data{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	auto result = data |
+				  views::filter([](int i) { return i % 2 == 1; }) |
+				  views::transform([](int i) { return to_string(i); });
+
+	for (auto v : result)
+		cout << v << ", ";
+
+	cout << endl;
+}
